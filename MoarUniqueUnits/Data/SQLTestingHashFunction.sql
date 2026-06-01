324a5ee181b541b8a696f00e463dafe03d564403
vanadium_zz <105497687@qq.com>
2026-06-01 20:42:45 +0800

    remove SaT

--- MoarUniqueUnits/Data/SQLTestingHashFunction.sql
+++
@@ -1,4 +0,0 @@
-
-DROP TRIGGER OnTypeInsert;
-CREATE TRIGGER OnTypeInsert AFTER INSERT ON Types BEGIN UPDATE Types SET Hash = random() Where Type = New.Type; END;
-PRAGMA foreign_keys = ON;
--- modinfoCombiner.py
+++ modinfoCombiner.py
@@ -31,7 +31,7 @@ def get_mod_folders():
         'Meso/Argentina',
         'Meso/MayaTeKinichII',
         'Meso/TheoticuanasTeotihuacan',
-        'MoarUniqueUnits',
+        # 'MoarUniqueUnits',
         'NorthAmerica/ThuleKiviuq',
         'NorthAmerica/CCBThuleWhalemaker',
         'Platforms',
